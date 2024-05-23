34250ca5d6a3119b1dc3b2693fea3037d79d4ef1
marufsiyam91 <marufsiyam9123@gmail.com>
2024-05-23 16:18:40 +0600

    place order cart total section completed

--- src/Pages/PlaceOrder.jsx
+++ src/Pages/PlaceOrder.jsx
@@ -8,9 +8,8 @@ const PlaceOrder = () => {
   const {totalAmount} = useContext(FoodieContext)
 
   return (
-    <div className="max-w-screen-4xl mx-auto py-20">
-      <form className="flex justify-between">
-        <div className="flex flex-col gap-4 basis-1/2">
+    <div className="max-w-screen-4xl mx-auto py-20 flex justify-between">
+      <form className="flex justify-between flex flex-col gap-4 basis-1/2">
           <p className="text-2xl text-black font-urbanist font-semibold">Delivery Information</p>
           <div className="flex gap-4">
             <input className="w-full bg-white border p-2 rounded-md" type="text" placeholder="First Name" />
@@ -27,29 +26,31 @@ const PlaceOrder = () => {
             <input className="w-full bg-white border p-2 rounded-md" type="text" placeholder="Country" />
           </div>
           <input className="w-full bg-white border p-2 rounded-md" type="text" placeholder="Phone" />
-        </div>
         <div>
-        <div className="basis-1/2">
-            <h3 className="text-2xl font-semibold font-urbanist">Cart Total</h3>
-            <div className="flex flex-col gap-2">
-              <div className="flex justify-between border-b">
-                <p>Subtotal</p>
-                <p>{totalAmount()}</p>
-              </div>
-              <div className="flex justify-between border-b">
-                <p>Delivery fee</p>
-                <p>2</p>
-              </div>
-              <div className="flex justify-between ">
-                <p className="text-xl font-semibold">Total</p>
-                <p className="text-xl font-semibold">{totalAmount() + 2}</p>
-              </div>
-            </div>
-
-            <button className="py-3 px-8  bg-orange-600 text-white font-semibold rounded-md mt-6">PROCEED TO PAYMENT</button>
-          </div>
         </div>
       </form>
+
+      <div className="basis-1/2 w-full flex justify-end">
+          <div className="w-2/3 ">
+            <h3 className="text-2xl font-semibold font-urbanist mb-6">Cart Total</h3>
+              <div className="flex flex-col gap-2">
+                <div className="flex justify-between border-b">
+                  <p>Subtotal</p>
+                  <p>$ {totalAmount()}</p>
+                </div>
+                <div className="flex justify-between border-b">
+                  <p>Delivery fee</p>
+                  <p>$ 2</p>
+                </div>
+                <div className="flex justify-between ">
+                  <p className="text-xl font-semibold">Total</p>
+                  <p className="text-xl font-semibold">$ {totalAmount() + 2}</p>
+                </div>
+              </div>
+
+              <button className="py-3 px-8  bg-orange-600 text-white font-semibold rounded-md mt-10">PROCEED TO PAYMENT</button>
+          </div>
+      </div>
     </div>
   )
 }
